b116c8b84d6af3c12fd9def4002f9f118a01b6c7
Michał Górny <mgorny@gentoo.org>
2019-10-05 18:48:24 +0200

    update-config: Make removing old keys optional
    Signed-off-by: Michał Górny <mgorny@gentoo.org>

--- update-config.py
+++ update-config.py
@@ -11,6 +11,8 @@ import sys
 
 def main(argv):
     argp = argparse.ArgumentParser(prog=argv[0])
+    argp.add_argument('--delete-old', action='store_true',
+                      help='Delete old options (kept by default)')
     argp.add_argument('path',
                       help='Path to .conf file')
     args = argp.parse_args(argv[1:])
@@ -23,7 +25,10 @@ def main(argv):
     with open(args.path, 'r') as f:
         conf = json.load(f)
 
+    if not args.delete_old:
+        all_classes.extend(conf.keys())
     new_conf = dict((k, conf.get(str(k), '')) for k in all_classes)
+
     with open(args.path, 'w') as f:
         json.dump(new_conf, f, sort_keys=True, indent=4)
 
