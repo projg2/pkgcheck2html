56dfb53d3772de05e2aedc018b5929f3d7a04d52
Michał Górny <mgorny@gentoo.org>
2019-10-05 18:45:33 +0200

    update-config: Add argparser
    Signed-off-by: Michał Górny <mgorny@gentoo.org>

--- update-config.py
+++ update-config.py
@@ -1,26 +1,34 @@
 #!/usr/bin/env python
 # vim:se fileencoding=utf8 :
-# (c) 2015-2017 Michał Górny
+# (c) 2015-2019 Michał Górny
 # 2-clause BSD license
 
+import argparse
 import json
 import subprocess
 import sys
 
 
-def main(json_path):
+def main(argv):
+    argp = argparse.ArgumentParser(prog=argv[0])
+    argp.add_argument('path',
+                      help='Path to .conf file')
+    args = argp.parse_args(argv[1:])
+
     s = subprocess.Popen(['pkgcheck', 'show', '--keywords'],
             stdout=subprocess.PIPE)
     sout, serr = s.communicate()
     all_classes = sout.decode().split()
 
-    with open(json_path, 'r') as f:
+    with open(args.path, 'r') as f:
         conf = json.load(f)
 
     new_conf = dict((k, conf.get(str(k), '')) for k in all_classes)
-    with open(json_path, 'w') as f:
+    with open(args.path, 'w') as f:
         json.dump(new_conf, f, sort_keys=True, indent=4)
+
+    return 0
 
 
 if __name__ == '__main__':
-    sys.exit(main(*sys.argv[1:]))
+    sys.exit(main(sys.argv))
